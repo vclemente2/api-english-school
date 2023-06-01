5ce4011b1ccd530415c7cd493c35b0d1d350914b
Vinicius Bastos <vclemente2@hotmail.com>
2023-06-01 18:32:06 -0300

    Implementação do ORM Sequelize e criação do model Pessoas.

---
+++ src/connection/migrations/20230601165446-create-pessoas.js
@@ -0,0 +1,41 @@
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Pessoas', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      nome: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      ativo: {
+        allowNull: false,
+        type: Sequelize.BOOLEAN
+      },
+      email: {
+        allowNull: false,
+        unique: true,
+        type: Sequelize.STRING
+      },
+      role: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface) {
+    await queryInterface.dropTable('Pessoas');
+  }
+};
